e6b9fda002f10e851f18b29a267fa2e9a01e7a3f
amandasqh <34236882+amandasqh@users.noreply.github.com>
2021-11-02 04:31:39 +0800

    fixed home content, left with responsive navbar

--- js/testhomepage.js
+++ js/testhomepage.js
@@ -36,7 +36,9 @@ const app = Vue.createApp({
                     cardImg: "images/homepage/basic_personality.svg",
                     link: "basicpersonalityloading.html",
                     desc: "offers Personality Analysis based on your music taste",
-                    color: "#78E185"
+                    color: "#78E185",
+                    id: "basic",
+                    isCompleted: true
                 },
                 "personality": {
                     title: "Personality Quiz",
@@ -44,7 +46,9 @@ const app = Vue.createApp({
                     cardImg: "images/homepage/personality_quiz_done.svg",
                     link: "personalityquiz.html",
                     desc: "offers further insights into which of the big 5 personalities you are",
-                    color: "#FF4B40"
+                    color: "#FF4B40",
+                    id: "personality",
+                    isCompleted: false
                 },
                 "career": {
                     title: "Career Quiz",
@@ -52,43 +56,55 @@ const app = Vue.createApp({
                     cardImg: "images/homepage/career_quiz_done.svg",
                     link: "careerquiz.html",
                     desc: "offers guidance on your suitable career prospects",
-                    color: "#37C1FF"
+                    color: "#37C1FF",
+                    id: "career",
+                    isCompleted: false
                 },
             },
 
             user: {
                 // supposed to retrieve from firebase...
-                username: "test",
+                username: "user",
                 charId: 1
             },
 
             completedQuizzes: 0
         } 
-    }
+    },
 
-    // methods: {
+    methods: {
+        countQuiz() {
+            let count = 0
+            for(elem in this.homeEl) {
+                let element = this.homeEl[elem]
+                if (element.isCompleted == true) {
+                    count++
+                }
+            }
+            this.completedQuizzes = count
+            // console.log(count)
+        }
     //     getUser(username, charId, imgSrc) {
     //         this.username = username
     //         this.charId = charId
     //         this.imgSrc = imgSrc
     //     }
-    // }
+    }
 });
 
 // Side Nav Bar Component
 app.component('nav-bar', {
     props: ['elements', 'user'],
     template: `
-        <nav class="nav flex-column navbar-dark bg-dark" id="navbar">
-            <div class="d-flex align-self-center" id="header">
-                <div id="cropped">
-                    <img :src="'images/upperbody/'+ user.charId +'.png'" id="icon"> 
-                </div>
-                <h2 id="logo" class="text-white" style="font-size: 1.8vw; padding: 2vw 0 0 1vw">Personalify</h2>
-            </div>
-            <div v-for="nav in elements" class="d-flex justify-content-start align-items-center" id="navlinks">
-                <i :class="nav.iconClass"> </i>
-                <a class="nav-link" :class="nav.isActive ? 'active fw-bold' : ''" :href="nav.link">
+        <nav class="nav flex-column navbar-dark bg-dark px-1" id="navbar">
+            <a class="text-center" id="header" href="">
+                <h2 id="logo" class="lightgreen pt-3" style="font-size: 2vw;">
+                    Personalify
+                </h2>
+            </a>
+            <div v-for="nav in elements" class="d-flex justify-content-center align-items-center" id="navlinks">
+                <a class="nav-link" :class="nav.isActive ? 'active activeNavLink' : ''" :href="nav.link">
+                    <i :class="nav.iconClass" class="pe-2 ps-0"> </i>
                     {{nav.title}}
                 </a>
             </div>
@@ -96,44 +112,43 @@ app.component('nav-bar', {
     `
 })
 
-app.component("header", {
-    template: 
-    `
-    <div id='heading'>
-        <span id='heading1'>Hello</span>
-        <br>
-        <span id='heading2'>See current quiz report or take a new one... </span>
-        <br>
-        <span id='heading3'>...do whatever you want I guess</span>
-    </div>
+app.component("header-content", {
+    props: ['user'],
+    template: `
+        <div class="text-white" id="welcomeText">
+            <h1 class="fw-bold">Hello, {{user.username}}</h1>
+            <h4 class="ps-3">See current quiz report or take a new one... </h4>
+            <h4 class="ps-3 fst-italic">...do whatever you want I guess</h4>
+        </div>
     `
 })
 
 app.component('home-content', {
     props: ['elements'],
     template: `
-    <div id="gridCards" class="container grid" v-for="el in elements">
-        <a class="grid__item grid__item--noclick" :href="el.link">
-            <div class="box p-0">
-                <div class="box__shadow"></div>
-                <img class="box__img" id='cardImg' :src="el.cardImg">
-                <h3 class="box__title">
-                    <span class="box__title-inner" data-hover="Level">
-                        Level {{el.level}}
-                    </span>
-                </h3>
-                <h4 class="box__text">
-                    <span class="box__text-inner box__text-inner--rotated2 box__text-inner--reverse">
-                        Level {{el.level}}
-                    </span>
-                </h4>
-                <p class="box__content">
-                    <strong>{{el.title}}</strong> 
-                    {{el.dec}}
-                </p>
-            </div>
-        </a>
-    </div>
+        <div id="gridCards" v-for="el in elements">
+            <a class="grid__item grid__item--noclick" :href="el.link" :id="el.id" :class="el.isCompleted ? 'quizActive' : 'quizInActive'">
+                <div class="box p-0">
+                    <div class="box__shadow">
+                    </div>
+                    <img class="box__img" id='cardImg' :src="el.cardImg">
+                    <h3 class="box__title">
+                        <span class="box__title-inner" data-hover="Level">
+                            Level {{el.level}}
+                        </span>
+                    </h3>
+                    <h4 class="box__text">
+                        <span class="box__text-inner box__text-inner--rotated2 box__text-inner--reverse">
+                            Level {{el.level}}
+                        </span>
+                    </h4>
+                    <p class="box__content">
+                        <strong>{{el.title}}</strong> 
+                        {{el.dec}}
+                    </p>
+                </div>
+            </a>
+        </div>
     `
 })
 
@@ -147,7 +162,7 @@ app.component('progress-bar', {
                 <div class="ocean">
                     <img :src="'images/upperbody/'+ user.charId +'.png'" id="upperBody">
                 </div> 
-            </div
+            </div>
             <div class="col-8">
                 <div id="progressbar">
                 </div>
