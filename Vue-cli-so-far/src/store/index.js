23eb43f89a27a58b121174572a153adbca9e479a
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2021-11-05 14:50:53 +0800

    Update index.js
    moved state into defaultState.js
    
    removed genres for music features
    
    added currentDate checking
    
    retrieval of music features for basic personality
    
    new action for retrieval of personality quiz from firebase using REST API

--- Vue-cli-so-far/src/store/index.js
+++ Vue-cli-so-far/src/store/index.js
@@ -1,30 +1,16 @@
 import { createStore } from "vuex";
 import createPersistedState from "vuex-persistedstate";
-import { cookieStore } from "@/components/persistedStores.js";
+// eslint-disable-next-line
+import { cookieStore, lsStore, sessionStore } from "@/components/vuex/persistedStores.js";
 import { newConfig, axiosAwait } from "@/components/getPost.js";
+// eslint-disable-next-line
+import { defaultState } from "@/components/vuex/defaultState.js";
 
 export default createStore({
-  plugins: [createPersistedState({ storage: cookieStore })],
-  state: {
-    isLoggedIn: false, // if user is logged in or not
-    myDomain: "", // web domain
-    spotifyDomain: "https://api.spotify.com/v1", // spotify api domain
-    myDatabase:
-      "https://wad2-941f5-default-rtdb.asia-southeast1.firebasedatabase.app",
-    timeLimit: 30, // 86400s = 1 day, currently 30 days
-    currentDate: "", // current date when user's tracks were checked
-
-    // user-to-store-data
-    nickname: "",
-    userId: "", // user ID spotify
-    genres: {}, // genre ids -- obj, cant use keyed collection like set
-    character: 0, // character chosen (0 for now since none chosen)
-    personalityType: "", // introvert, extrovert or ambivert
-
-    // personality quiz
-    personalityQuestions: {}, // questions object, key: trait, value: qn arr
-    personalityResults: {}, // results obj, key: trait, value: points
-  },
+  // persisted state for persisted vuex store in the browser
+  plugins: [createPersistedState({ storage: sessionStore() })],
+  // state now fully added outside the vuex store file
+  state: defaultState(), // can either be {} or defaultState()
   mutations: {
     updateLoginState(state, isIn) {
       state.isLoggedIn = isIn;
@@ -38,9 +24,9 @@ export default createStore({
     updateId(state, newId) {
       state.userId = newId;
     },
-    updateGenres(state, newGenre) {
-      console.log(newGenre);
-      state.genres[newGenre] = 0; // check for newGenre in state.genres doesn't work, doesn't really check it, and I can't find why
+    updateCurrentDate(state, newCurrentDate) {
+      // in preparation for storing the current date
+      state.currentDate = newCurrentDate;
     },
     updateCharacter(state, newCharacter) {
       state.character = newCharacter;
@@ -51,36 +37,80 @@ export default createStore({
     updatePersonalityQuestions(state, newPersonalityQuestions) {
       state.personalityQuestions = newPersonalityQuestions;
     },
+    updatePersonalityResults(state, newPersonalityResults) {
+      state.personalityResults = newPersonalityResults;
+    },
+    // the values for personality would be: totalMusicFeatures / numOfTracks
+    updateTotalMusicFeatures(state, newMusicFeatures) {
+      for (let smf in state.totalMusicFeatures) {
+        state.totalMusicFeatures[smf] += newMusicFeatures[smf];
+      }
+    },
+    updateNumOfTracks(state) {
+      state.numOfTracks += 1;
+    },
   },
   actions: {
+    // name is a bit outdated, but we can still roll with it for the time being
     async retrieveGenres({ state, commit }, auth_token) {
       const singleDay = 86400; // one day is 86400 seconds
-      const recentTime =
-        Math.floor(Date.now() / 1000) - state.timeLimit * singleDay;
+      // as timeLimit is 30 days, timeBetween is roughly a month
+      const timeBetween = state.timeLimit * singleDay;
+      const currentTime = Math.floor(Date.now() / 1000);
+      const currentDate = new Date();
+      commit("updateCurrentDate", currentDate);
+      const recentTime = currentTime - timeBetween;
 
+      // retrieving the tracks of the users
       const config = newConfig(
         "GET",
         `${state.spotifyDomain}/me/player/recently-played?limit=50&after=${recentTime}`,
         { auth: `Bearer ${auth_token}` }
       );
       const tracks = await axiosAwait(config);
+      // if tracks is retrieved
       if (tracks) {
-        let userArtists = new Set();
+        // create set for userArtists and also for userTracks
+        const userArtists = new Set();
+        const userTracks = new Set();
+
+        // for each track object
         tracks.items.forEach((item) => {
+          // saving the artist and tracks into respective sets
+          userTracks.add(item.track.id);
           item.track.artists.forEach((artist) => userArtists.add(artist.id));
         });
-        userArtists.forEach(async (id) => {
-          let artistConfig = newConfig(
+
+        // audio features -- alternative to genres
+        // for every track
+        userTracks.forEach(async (id) => {
+          const audioConfig = newConfig(
             "GET",
-            `${state.spotifyDomain}/artists/${id}`,
+            `${state.spotifyDomain}/audio-features/${id}`,
             { auth: `Bearer ${auth_token}` }
           );
-          let artist = await axiosAwait(artistConfig);
-          if (artist)
-            artist.genres.forEach((genre) => commit("updateGenres", genre));
-          else console.log("Unable to retrieve genres");
+          const features = await axiosAwait(audioConfig);
+          if (features) {
+            // only want certain parts to be saved
+            commit("updateNumOfTracks");
+            commit("updateTotalMusicFeatures", features);
+          } else {
+            console.log("Cant get audio features");
+          }
         });
       } else console.log("Unable to get tracks");
+    },
+    async retrievePersonalityQuiz({ state, commit }) {
+      // will only call when personalityQuiz is empty
+      const config = newConfig(
+        "GET",
+        `${state.myDatabase}/quizzes/personalityQuiz/questions.json`
+      );
+      const db_quiz = await axiosAwait(config);
+      if (db_quiz) {
+        console.log(db_quiz);
+        commit("updatePersonalityQuestions", db_quiz);
+      } else console.log("Unable to retrieve personality questions");
     },
   },
 });
