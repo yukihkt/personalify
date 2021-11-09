bd870e4b9226d0a9a34d0b8beb821ed2baf2656b
yukihkt <yuki.han.2020@scis.smu.edu.sg>
2021-11-09 19:16:28 +0800

    pushed to update vue carousel

--- Vue-cli-so-far/src/views/PersonalityQuiz.vue
+++ Vue-cli-so-far/src/views/PersonalityQuiz.vue
@@ -1,10 +1,11 @@
 <template>
-  <Sidebar />
+
 
   <div
     style="margin-left: 18rem; margin-right: 0"
     class="row d-flex justify-content-center px-4 pb-3"
   >
+    <Sidebar />
     <div class="col">
       <!-- start of the quiz component -->
       <div v-if="!quizDone">
@@ -13,12 +14,12 @@
           Personality Quiz
         </div>
         <hr />
-        <div id="main-content-areas" class="mt-1 rounded-3 bg-pinkred">
-          <div>
-            <vueper-slides :dragging-distance="70" prevent-y-scroll>
+        <div id="main-content-areas">
+          <div class = "container-fluid" :style= "slideOffset">
+            <vueper-slides class = "no-shadow" :class="{ 'offset-slide--first': firstSlide, 'offset-slide--last': lastSlide }" before-slide="beforeSlide" :visible-slides="3" :slide-ratio="1/4" :dragging-distance = "75" :arrows-outside="false" show-multiple-slides>
               <!-- v-if="!showQuiz" -- not sure if to have or not -->
-              <vueper-slide>
-                <!-- initial slide, welcome type-message -->
+              <!-- <vueper-slide>
+                initial slide, welcome type-message 
                 <template #content>
                   <div class="vueperslide__content-wrapper">
                     <div>Personality Quiz</div>
@@ -33,25 +34,17 @@
                     </div>
                   </div>
                 </template>
-              </vueper-slide>
+              </vueper-slide> -->
               <!-- main question slides -->
               <!-- eslint-disable-next-line vue/no-use-v-if-with-v-for prettier/prettier -->
-              <vueper-slide v-for="slide in slides" :key="slide.id" v-if="showQuiz">
+              <!-- <vueper-slide v-for="slide in slides" :key="slide.id" v-if="showQuiz"> -->
+              <vueper-slide v-for="slide in slides" :key="slide.id" style = "width:100%;">
                 <template #content>
-                  <div class="vueperslide__content-wrapper">
+                  <!-- <div class="vueperslide__content-wrapper"> -->
                     <div>{{ slide.title }}</div>
                     <div class="flex-row mt-2">
                       <span class="m-3">
-                        <input
-                          class="form-check-input mx-1"
-                          type="radio"
-                          :id="slide.id"
-                          :name="slide.id"
-                          :value="0"
-                          v-model="personalityAnswers[slide.id]"
-                        /><label :for="slide.id">Not Me</label>
-                      </span>
-                      <span class="m-3">
+                        <button class="btn btn-dark">
                         <input
                           class="form-check-input mx-1"
                           type="radio"
@@ -60,9 +53,22 @@
                           :value="1"
                           v-model="personalityAnswers[slide.id]"
                         /><label :for="slide.id + 1">That's Me</label>
+                        </button>
+                      </span>
+                      <span class="m-3">
+                        <button class="btn btn-dark">
+                        <input
+                          class="form-check-input mx-1"
+                          type="radio"
+                          :id="slide.id"
+                          :name="slide.id"
+                          :value="0"
+                          v-model="personalityAnswers[slide.id]"
+                        /><label :for="slide.id">Not Me</label>
+                        </button>
                       </span>
                     </div>
-                  </div>
+                  <!-- </div> -->
                 </template>
               </vueper-slide>
               <vueper-slide v-if="Object.keys(personalityAnswers).length == 15">
@@ -98,7 +104,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useStore } from "vuex";
 import { checkDomain } from "../components/redirect";
 import Sidebar from "../components/sidebar/Sidebar.vue";
@@ -126,6 +132,21 @@ export default {
       categories.push(category);
     }
 
+    const firstSlide = 0
+    const lastSlide = 15
+
+    const beforeSlide = ({nextSlide}) => {
+      this.firstSlide = !nextSlide.index
+      this.lastSlide = nextSlide.index === this.slides.length - 1
+      console.log(lastSlide)
+    }
+
+    const slideOffset = computed(function () {
+      return {
+        '--offset': this.currentSlide
+      }
+    });
+
     // v-modeled question answers
     const personalityAnswers = ref({});
 
@@ -134,7 +155,7 @@ export default {
     const quizDone = ref(false);
 
     const results = store.state.personalityResults;
-
+    
     // only shows when all questions are answered
     const submitQuiz = () => {
       quizDone.value = true;
@@ -157,24 +178,58 @@ export default {
       submitQuiz,
       slides,
       personalityAnswers,
+      firstSlide,
+      lastSlide,
+      beforeSlide,
+      slideOffset
     };
   },
 };
 </script>
 
-<style scoped>
+<style scoped lang = "scss">
+input {
+  width: 0%;
+  height: 0%;
+}
+
 .btn {
   width: 8rem;
   height: 3rem;
   margin: 0 0.5rem 0;
 }
+
 .submit-btn {
   width: 12rem;
   height: 4rem;
 }
-.bg-pinkred {
-  background-color: hsl(300, 69%, 70%);
+
+.vueperslides__arrow {color: whitesmoke;}
+.vueperslides__bullet {border-color: #E73C7E;box-shadow: none;}
+
+.vueperslide {
+  color: #fff;
+  background: red;
+  transition: .3s ease-in-out;
+  opacity: 0.3;
+  /* filter: blur(4px); */
+  transform: scale(0.9);
 }
+
+.veuperslide--active {
+    opacity: 1 !important;
+    transform: scale(1);
+    filter: blur(0);
+}
+
+.offset-slide--first .vueperslides__track-inner {
+  transform: translate3d(33.33%, 0, 0) !important;
+}
+
+.offset-slide--last .vueperslides__track-inner {
+  transform: translate3d(-133.33%, 0, 0) !important;
+}
+
 /* .vueperslides__bullet span {
   display: block;
   color: #fff;
