0cc6f59697664d755e9a36b6f4b24c93f73d0273
amandasqh <34236882+amandasqh@users.noreply.github.com>
2021-11-10 02:17:34 +0800

    flip cards

--- js/about.js
+++ js/about.js
@@ -2,18 +2,36 @@
 app.component("about-content", {
     props: ['names', 'covers'],
     template: `
-    <div id="about" class="container-fluid text-center">
-        <div class="row justify-content-center" id="team">
-            <h3> Meet Our Team </h3>
+    <div id="about" class="container-fluid pt-3 ">
+        <div id="team" class="row justify-content-center">
+            <h2> Meet Our Team </h2>
             <div class="col-lg-2 col-md-6 col-12" v-for="(value,key) in names">
-                <img :src="'images/about/'+ key +'.svg'">
+                <div class="card">
+                    <div class="content">
+                        <div class="front">
+                            <img :src="'images/about/'+ key +'.svg'">
+                        </div>
+                        <div class="back">
+                            {{value.role}}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
-        <div id="covers" class="row justify-content-center">
-            <h3> About <span class="lightgreen"> Personalify </span> </h3>
-            <div class="col-lg-4 col-md-6 col-12" v-for="cover in covers">
-                <img :src="cover.cardImg">
+        <div class="row justify-content-center">
+            <h2> About <span class="lightgreen"> Personalify </span> </h2>
+            <div id="covers" class="col-lg-3 col-md-6 col-12" v-for="cover in covers">
+                <div class="card">
+                    <div class="content">
+                        <div class="front">
+                            <img :src="cover.cardImg">
+                        </div>
+                        <div class="back">
+                            {{cover.full_desc}}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
