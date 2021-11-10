f8362bbcf903d4d4a35395959d0ddb4f0f3d4955
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2021-11-11 04:32:28 +0800

    Update MusicBasic.vue
    One of the components that are dynamically switched onto the MainPage, labeled as Level 1.
    
    Chart js radar, and also shows other 5 variable averaged data retrieved from the spotify api based on the user's choice of songs in the last X days
    (X = number of days, can be changed under the "retrieveGenres" action in store/index.js)

--- Vue-cli-so-far/src/components/main/MusicBasic.vue
+++ Vue-cli-so-far/src/components/main/MusicBasic.vue
@@ -1,19 +1,35 @@
 <template>
-  <!-- some space for the music basic area, this is a component -->
-  <div class="container-fluid text-start mt-2">
-    <button @click="returnToMain" class="btn btn-success">
-      Return to main
-    </button>
-    <ul>
-      <li v-for="(value, key) in avgMusicFeatures" :key="key">
-        {{ key }}: {{ value }}
-      </li>
-    </ul>
+  <div>
+    <!-- some space for the music basic area, this is a component -->
+    <div id="chart1" class="mt-3 container bglightgreen rounded-3">
+      <canvas ref="featureChart"></canvas>
+    </div>
+    <div
+      class="
+        mt-2
+        rounded-3
+        bg-success
+        row row-cols-auto
+        d-flex
+        justify-content-center
+      "
+      style="width: 80vw"
+    >
+      <div
+        class="col-12 col-lg-6 col-xl-4 py-2"
+        v-for="(value, feature) in nonChart"
+        :key="feature"
+      >
+        {{ feature }}: {{ value }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { useStore } from "vuex";
+import Chart from "chart.js/auto";
+import { ref, onMounted } from "vue";
 
 export default {
   name: "MusicBasic",
@@ -24,17 +40,60 @@ export default {
 
     const { totalMusicFeatures, numOfTracks } = store.state;
 
-    let avgMusicFeatures = {};
+    const nonChart = { loudness: 0, tempo: 0, duration_ms: 0, key: 0, mode: 0 };
+    const chartFeatures = {};
     for (let feature in totalMusicFeatures) {
-      avgMusicFeatures[feature] = totalMusicFeatures[feature] / numOfTracks;
+      const avgMusicFeatures = (
+        totalMusicFeatures[feature] / numOfTracks
+      ).toFixed(2);
+      if (!(feature in nonChart)) chartFeatures[feature] = avgMusicFeatures;
+      else nonChart[feature] = avgMusicFeatures;
     }
-    console.log(avgMusicFeatures);
+
+    const featureChart = ref(null);
+    onMounted(() => {
+      // eslint-disable-next-line
+      const myChart1 = new Chart(featureChart.value, {
+        type: "radar",
+        data: {
+          labels: Object.keys(chartFeatures), // array of music features labels
+          datasets: [
+            {
+              label: "Your Music Traits",
+              data: Object.values(chartFeatures),
+              fill: true,
+              backgroundColor: "rgba(33, 140, 49, 0.3)",
+              borderColor: "rgb(33, 140, 49)", // .bgdarkgreen color scheme
+              pointBackgroundColor: "rgb(33, 140, 49)",
+              pointBorderColor: "#fff",
+              pointHoverBackgroundColor: "#fff",
+              pointHoverBorderColor: "rgb(33, 140, 49)",
+            },
+          ],
+        },
+        options: {
+          elements: {
+            line: {
+              borderWidth: 2,
+            },
+          },
+        },
+      });
+    });
 
     const returnToMain = () => emit("to-other-component");
 
-    return { returnToMain, avgMusicFeatures };
+    return { returnToMain, featureChart, nonChart };
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn {
+  width: 9.5rem;
+  height: 3rem;
+}
+#chart1 {
+  width: 96%;
+}
+</style>
