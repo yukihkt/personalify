5a7fa761fcb035c87f93b4eab1544e3b7a28483b
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2021-11-11 04:36:03 +0800

    minor update to the logout function
    removing unnecessary code

--- Vue-cli-so-far/src/components/sidebar/Sidebar.vue
+++ Vue-cli-so-far/src/components/sidebar/Sidebar.vue
@@ -1,22 +1,13 @@
 <template>
   <!-- sidebar component -->
   <div id="sidebar">
-    <!-- img or something if we have one, afaik we don't -->
-    <span id="logo">
-      <i class="bi bi-person-circle" />
-      Personalify
-    </span>
-
+    <span id="logo" class="text-decoration-underline">Personalify</span>
     <!-- main navigation cluster -->
     <SidebarLink to="/main" icon="fas fa-home">Home</SidebarLink>
     <SidebarLink to="/about" icon="fas fa-info-circle">About</SidebarLink>
-    <SidebarLink to="/report" icon="fas fa-file-alt">Report</SidebarLink>
-
-    <!-- need to somehow pass in the attribute of "replace into this one" -->
-    <!-- not sure how I will do so, but it is needed for my plan -->
-    <SidebarLink to="/" icon="fas fa-sign-out-alt" @click="logout">
-      Logout
-    </SidebarLink>
+    <!-- <SidebarLink to="/report" icon="fas fa-file-alt">Report</SidebarLink> -->
+    <!-- no need replace, since the user's local store will get reset automatically anyways -->
+    <SidebarLink to="/" icon="fas fa-sign-out-alt">Logout</SidebarLink>
   </div>
 </template>
 
@@ -27,14 +18,7 @@ export default {
   name: "Sidebar",
   components: { SidebarLink },
   setup() {
-    const logout = () => {
-      // this function routes the user back to the home page,
-      // data that can be saved should be routed into the firebase store,
-      // the click should clear the vuex persisted store in the process
-      // and reset the user login state, so the user can leave the thing
-      console.log("logout function");
-    };
-    return { logout };
+    return {};
   },
 };
 </script>
